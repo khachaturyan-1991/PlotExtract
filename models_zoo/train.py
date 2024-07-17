2b6e9e2ab7dd1671ac4cab771c93f787578d266f
khachaturyan-1991 <dr.khachaturyan@gmail.com>
2024-07-17 20:42:18 +0200

    added model_zoo and UNet with customizable depth

---
+++ models_zoo/train.py
@@ -0,0 +1,128 @@
+import torch
+from torch import nn
+import tqdm
+
+import mlflow
+import mlflow.pytorch
+
+import matplotlib.pylab as plt
+
+
+class Trainer:
+
+    def __init__(self,
+                 model: nn.Module,
+                 loss_fn: nn.Module,
+                 optimizer: torch.optim,
+                 device: str = "cpu:0",
+                 tb_log_dir: str = "./logs"
+                 ) -> None:
+        self.model = model
+        self.device = device
+        self.loss_fn = loss_fn
+        self.model
+        self.optimizer = optimizer
+
+    def log_weights_and_gradients(self, step):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                mlflow.log_metric(f"{name}_weight_mean", param.data.mean().item(), step=step)
+                mlflow.log_metric(f"{name}_weight_std", param.data.std().item(), step=step)
+                if param.grad is not None:
+                    mlflow.log_metric(f"{name}_grad_mean", param.grad.mean().item(), step=step)
+                    mlflow.log_metric(f"{name}_grad_std", param.grad.std().item(), step=step)
+
+    def get_gradients(self):
+        gradients = []
+        for param in self.model.parameters():
+            if param.grad is not None:
+                gradients.append(param.grad.clone().detach())
+            else:
+                gradients.append(torch.zeros_like(param))
+        return torch.tensor([torch.mean(torch.abs(grad)) for grad in gradients])
+
+    def train_step(self,
+                   dataloader: torch.utils.data.DataLoader
+                   ):
+        self.model.train()
+        step_loss = 0
+        n = 0
+        for X, mask in dataloader:
+            X = X.to(self.device)
+            mask = mask.to(self.device)
+            pred = self.model(X)
+            loss = self.loss_fn(pred, mask)
+            step_loss += loss.item()
+            loss.backward()
+            self.optimizer.step()
+            n += 1
+        return step_loss / n
+
+    def test_step(self,
+                  dataloader: torch.utils.data.DataLoader,
+                  ):
+        self.model.eval()
+        step_loss = 0
+        n = 0
+        for X, mask in dataloader:
+            X = X.to(self.device)
+            mask = mask.to(self.device)
+            pred = self.model(X)
+            loss = self.loss_fn(pred, mask)
+            step_loss += loss.item()
+            n += 1
+        return step_loss / n
+
+    def fit(self,
+            train_dataloder: torch.utils.data.DataLoader,
+            test_dataloder: torch.utils.data.DataLoader,
+            output_freq: int = 2,
+            first_step: int = 0,
+            epochs: int = 10):
+        with mlflow.start_run() as _:  # it was run
+            last_step = first_step + epochs
+            avg_train_loss = {}
+            avg_val_loss = {}
+            for epoch in tqdm.tqdm(range(epochs)):
+                train_loss = self.train_step(train_dataloder)
+                test_loss = self.test_step(test_dataloder)
+                avg_train_loss[first_step + epoch] = train_loss
+                avg_val_loss[first_step + epoch] = test_loss
+                if epoch % output_freq == 0:
+                    print(f"Epoch {epoch + 1 + first_step}/{first_step + last_step}, \
+                        Train loss: {train_loss:.4f} Validation loss: {test_loss:.4f}")
+                    mlflow.log_metric("train_loss", train_loss, step=epoch)
+                    mlflow.log_metric("validation_loss", test_loss, step=epoch)
+                    # self.log_weights_and_gradients(step=epoch)
+            mlflow.pytorch.log_model(self.model, "models")
+        return avg_train_loss, avg_val_loss
+
+
+def test_prediction(model,
+                    dataloader,
+                    loss_fn,
+                    image_name: str = "test_prediction"):
+    model.to("cpu")
+    loss = 0
+    n = 0
+    for X, masks in dataloader:
+        Y_pred = model(X)
+        loss += loss_fn(Y_pred, masks).item()
+        n += 1
+
+    print("Loss per test: ", loss / n)
+
+    Y_pred = torch.squeeze(Y_pred, dim=-1)
+    Y_pred = Y_pred.detach().numpy()
+    masks = masks.numpy()
+    _, axes = plt.subplots(2, 4, figsize=(8, 5))
+    axes = axes.ravel()
+    for i in range(4):
+        axes[i].imshow(Y_pred[i][0])
+        axes[i].axis("off")
+        y = masks[i]
+        y[y > 1] = 1
+        axes[i + 4].imshow(y[0])
+        axes[i + 4].axis("off")
+    plt.tight_layout()
+    plt.savefig(f"{image_name}.png")
---
+++ models_zoo/unet.py
@@ -0,0 +1,104 @@
+from torch.functional import F
+from torch import nn
+from torchinfo import summary
+
+
+CONV_KERNEL = (3, 3)
+POOL_KERNEL = (2, 2)
+STRIDE_SIZE = (1, 1)
+PADDING_TYPE = 1
+MODEL_DEPTH = 3
+
+
+class DownBlock(nn.Module):
+
+    def __init__(self,
+                 in_channels,
+                 out_channels, **kwargs):
+        super(DownBlock, self).__init__(**kwargs)
+
+        self.conv_1 = nn.Conv2d(in_channels=in_channels,
+                                out_channels=out_channels,
+                                kernel_size=CONV_KERNEL,
+                                padding=PADDING_TYPE)
+        self.batch_1 = nn.BatchNorm2d(num_features=out_channels)
+        self.conv_2 = nn.Conv2d(in_channels=out_channels,
+                                out_channels=out_channels,
+                                kernel_size=CONV_KERNEL,
+                                padding=PADDING_TYPE)
+        self.batch_2 = nn.BatchNorm2d(num_features=out_channels)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=POOL_KERNEL)
+
+    def forward(self, x):
+        x = self.conv_1(x)
+        x = self.batch_1(x)
+        x = self.relu(x)
+        x = self.conv_2(x)
+        x = self.batch_2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        return x
+
+
+class UpperBlock(nn.Module):
+
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int, **kwargs):
+        super(UpperBlock, self).__init__(**kwargs)
+        self.convT = nn.ConvTranspose2d(in_channels=in_channels,
+                                        out_channels=out_channels,
+                                        kernel_size=CONV_KERNEL,
+                                        stride=STRIDE_SIZE,
+                                        padding=PADDING_TYPE)
+        self.conv_1 = nn.Conv2d(in_channels=out_channels,
+                                out_channels=out_channels,
+                                kernel_size=CONV_KERNEL,
+                                stride=STRIDE_SIZE,
+                                padding=PADDING_TYPE)
+
+    def forward(self, x, w=None):
+        x = self.convT(x)
+        x = self.conv_1(x)
+        s = x.shape[2:]
+        x = F.interpolate(x,
+                          size=(s[0] * 2, s[1] * 2),
+                          mode='bilinear',
+                          align_corners=False)
+        return x
+
+
+class UNet(nn.Module):
+
+    def __init__(self, depth: int = MODEL_DEPTH, **kwargs):
+        super(UNet, self).__init__(**kwargs)
+        self.depth = depth
+        self.down_blocks = nn.ModuleDict()
+        self.down_blocks[str(0)] = DownBlock(1, 64)
+        n_feat = 64
+        for i in range(1, depth):
+            self.down_blocks[str(i)] = DownBlock(n_feat, 2 * n_feat)
+            n_feat *= 2
+
+        self.up_block = nn.ModuleDict()
+        n_feat = 64 * (2 ** (depth - 1)) // 2
+        for i in range(1, depth):
+            self.up_block[str(depth - i)] = UpperBlock(2 * n_feat, n_feat)
+            n_feat //= 2
+        self.up_block[str(0)] = UpperBlock(64, 1)
+
+    def forward(self, x):
+        x = self.down_blocks[str(0)](x)
+        for i in range(1, self.depth):
+            x = self.down_blocks[str(i)](x)
+        for i in range(1, self.depth):
+            x = self.up_block[str(self.depth - i)](x)
+        x = self.up_block[str(0)](x)
+        return x
+
+
+if __name__ == "__main__":
+
+    model = UNet(depth=3)
+    summary(model, input_size=(1, 1, 128, 128))

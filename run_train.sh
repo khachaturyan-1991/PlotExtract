03c5560075c982e4c29a674a38cabb3323dc225f
khachaturyan-1991 <dr.khachaturyan@gmail.com>
2024-08-22 14:08:32 +0200

    added to notebook firs CRNN based on LSTM

--- run_train.sh
+++ run_train.sh
@@ -4,5 +4,5 @@ testing_parameter=(0.8 0.95)
 
 for param in "${testing_parameter[@]}"; do
     echo "Running model with param value = ${param}"
-    python run.py --device mps --epochs 200 --output_freq 20 --depth 3 --fig_size 2 --img_size 128 --lr 3e-4 --dice_coef "$param"
+    python run_train.py --device mps --epochs 200 --output_freq 20 --depth 3 --fig_size 2 --img_size 128 --lr 3e-4 --dice_coef "$param"
 done

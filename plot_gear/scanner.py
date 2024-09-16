5136c9a32c2bd396cf1518c4774862f2b8592dfb
khachaturyan-1991 <dr.khachaturyan@gmail.com>
2024-09-16 21:21:28 +0200

    docstrings to the scanner in the gear folder

--- plot_gear/scanner.py
+++ plot_gear/scanner.py
@@ -31,12 +31,14 @@ class PlotScanner(PlotProcessor):
             self.models[model_names[i]] = model
 
     def _prepare_img(self, img):
+        """preprocess the input image"""
         img = np.transpose(img, (2, 0, 1)) / 255.0
         _, self.img = cv2.threshold(img, 0.9, 1, cv2.THRESH_BINARY)
         plt.imshow(np.transpose(img, (1, 2, 0)))
         plt.savefig(self.RES_FOLDER + "original.png")
 
     def _load_models(self):
+        """loads weights to the modesl"""
         DEPTH = 3
         unet_model = UNet(depth=DEPTH)
         unet_model = load_model(unet_model, "./pretrained/segmentation.pth")
@@ -46,6 +48,7 @@ class PlotScanner(PlotProcessor):
         return unet_model, crnn_models["x"], crnn_models["y"]
 
     def _segment(self):
+        """performs plots and labels segmentations"""
         segmented = self.img.copy()
         segmented = np.expand_dims(segmented, axis=0)
         segmented = torch.tensor(segmented, dtype=torch.float32)
@@ -60,6 +63,8 @@ class PlotScanner(PlotProcessor):
         self.segmented = segmented
 
     def _get_labels_positions(self):
+        """finds positions of labels (in pixels)
+            from segmentation results"""
         square = self.segmented[0][0].copy()
         label_extractor = RelateCoordinates(square)
         extracted_axes = label_extractor.get_uniform_positions()
@@ -70,6 +75,7 @@ class PlotScanner(PlotProcessor):
         return extracted_axes
 
     def _get_labels(self):
+        """crop x- and y-labels from an image to extract numbers on labels"""
         crops_lim = {"x": [270, 290], "y": [0, 30]}
         labels_nums = {}
         for i in "x y".split():
@@ -85,10 +91,13 @@ class PlotScanner(PlotProcessor):
         return labels_nums
 
     def _set_rescaler(self, labels_pos, labels_nums):
+        """set rescaler funciton that transforms coordintas
+            from pixesl to images coordinates"""
         rescaler = Rescaler(labels_pos, labels_nums)
         self.rescaler = rescaler
 
     def _create_tracker(self):
+        """intantiates tacker that separates plots from segmented lines"""
         plots_extracted = self.segmented[0][1].copy()
         tracker = Tracker()
         extractor = CCD(tracker, iniertia=0.9, velocity=1, accelaration=3)
@@ -99,18 +108,29 @@ class PlotScanner(PlotProcessor):
         for key in trace.keys():
             extracted_plots = np.array(trace[key])
             res = self.rescaler.rescale(extracted_plots)
-            ax[1].plot(res[:, 0], -res[:, 1])
+            ax[1].plot(res[:, 0], res[:, 1])
+            trace[key] = res
         ax[1].set_xlim(-2.2, 2.2)
         ax[1].set_ylim(-10.2, 10.2)
         plt.savefig(self.RES_FOLDER + "final.png")
+        return trace
 
     def handle(self, img):
+        """run all together:
+            1. prepare an image
+            2. segments plots and labels
+            3. finds labels positions (in pixels)
+            4. extract numebrs on axes
+            5. rescale pixels to images coordinates
+            6. applies tracker to separate plots
+        """
         img = self._prepare_img(img)
         self._segment()
         labels_pos = self._get_labels_positions()
         labels_nums = self._get_labels()
         self._set_rescaler(labels_pos, labels_nums)
-        self._create_tracker()
+        extracted_plots = self._create_tracker()
+        return extracted_plots
 
 
 if __name__ == "__main__":
